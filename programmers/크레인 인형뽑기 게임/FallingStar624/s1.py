0d68898e85a9dd2943c93c26267b85d9b23edcef
FallingStar624 <chlrhkdgh624@snu.ac.kr>
2021-10-21 04:54:38 +0900

    Upload: week1 | 크레인 인형뽑기 게임

--- programmers/크레인 인형뽑기 게임/FallingStar624/s1.py
+++ programmers/크레인 인형뽑기 게임/FallingStar624/s1.py
@@ -20,6 +20,7 @@ def solution(board, moves):
     return cnt
 
 
+
 arr1 = [[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]]
 arr2 = [1, 5, 3, 5, 1, 2, 1, 4]
 print(solution(arr1, arr2))
