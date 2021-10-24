3c1be1885d47c14372954d5329924f2f99e27411
JYA <abogado2113@gmail.com>
2021-10-24 12:18:09 +0900

    Upload: week1 | 더 맵게

---
+++ programmers/더 맵게/JYtorch/s1.py
@@ -0,0 +1,19 @@
+scoville = [1, 2, 3, 9, 10, 12]
+K = 7
+
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+
+    heapq.heapify(scoville)
+    while scoville[0] < K:
+        if len(scoville) > 1:
+            f1 = heapq.heappop(scoville)
+            f2 = heapq.heappop(scoville)
+            heapq.heappush(scoville, f1 + f2 * 2)
+            answer += 1
+        else:
+            return -1
+
+    return answer
