94d98b8c4e1ed2346e9f94139167409a1a5576b1
euijinbang <euijin.bang@gmail.com>
2021-11-30 23:10:37 +0900

    week2 commit

--- programmers/완주하지 못한 선수/euijin.bang/s3.py
+++ programmers/완주하지 못한 선수/euijin.bang/s3.py
@@ -1,9 +1,37 @@
-from collections import Counter
+# from collections import Counter
+#
+# def solution(participant, completion):
+#     print(Counter(participant))
+#     print(Counter(completion))
+#     answer = Counter(participant) - Counter(completion)
+#     return list(answer.keys())[0]
+#
+# print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
 
-def solution(participant, completion):
-    print(Counter(participant))
-    print(Counter(completion))
-    answer = Counter(participant) - Counter(completion)
-    return list(answer.keys())[0]
+s = {12: 1, 28: 1, 878:1}
+g = [1, 11, 1, 55, 12, 11, 14, 44] # movie_id
+r = []
 
-print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
+for genre_id, cnt in s.items(): # 12,1  28,1  878,1
+    c = 0
+    for i in range(len(g)):
+        movie = Movie.objects.get(id=g[i])
+        if genre_id in movie.genre_ids:
+            r.append(movie)
+            g.pop(i)
+            c += 1
+            if c == cnt:
+                break
+        else:
+            # 동일 장르이면서 평점(vote_average)가 높은 영화
+            alt = Movie.objects.filter(genre_ids__contains=genre_id).order_by('-vote_average')
+            r.append()
+            c += 1
+            if c == cnt:
+                break
+
+
+
+
+
+
