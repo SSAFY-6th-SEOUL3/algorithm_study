3c33f3d7a8bb8fceb184cedba5bb3e3884fb8783
euijinbang <euijin.bang@gmail.com>
2021-12-13 15:01:03 +0900

    Update: week8|가장 먼 노드

--- programmers/가장 먼 노드/euijin.bang/s1.py
+++ programmers/가장 먼 노드/euijin.bang/s1.py
@@ -1,10 +1,9 @@
 from collections import deque
 
-# 디버깅중....
 def solution(n, edge):
     visited = [0] * (n + 1)
     adj = [[] for _ in range(n + 1)]
-    for i in range(n):
+    for i in range(len(edge)):
         x, y = edge[i][0], edge[i][1]
         adj[x].append(y)
         adj[y].append(x)
@@ -19,8 +18,12 @@ def solution(n, edge):
                 visited[w] = visited[v] + 1
                 q.append(w)
 
-    return ...
-
+    # visited = [0, 1, 2, 2, 3, 3, 3]
+    counter = [0] * (max(visited)+1)
+    for i in range(n+1):
+        counter[visited[i]] += 1
+    # counter = [1, 1, 2, 3]
+    return counter[max(visited)]
 
 vertex = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
 print(solution(6, vertex))
