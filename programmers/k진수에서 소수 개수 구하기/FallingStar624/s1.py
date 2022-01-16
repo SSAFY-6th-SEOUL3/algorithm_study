00254591a063400c0bd5f83ea704e7e77dbdb0ae
FallingStar624 <chlrhkdgh624@snu.ac.kr>
2022-01-16 15:42:34 +0900

    Upload: Week13 | k진수에서 소수 개수 구ㄱ하기

---
+++ programmers/k진수에서 소수 개수 구하기/FallingStar624/s1.py
@@ -0,0 +1,26 @@
+# TC1 왜 시간초과?...
+def prime_check(number):
+    number = int(number)
+    if number == 1:
+        return 0
+    for i in range(2, (number // 2)+1):
+        if number % i == 0:
+            return 0
+    return 1
+
+
+def solution(n, k):
+    result = ""
+    while n > 0:
+        n, r = divmod(n, k)
+        result = str(r) + result
+    answer = result.split('0')
+    cnt = 0
+    for tmp in answer:
+        if tmp and prime_check(tmp):
+            cnt += 1
+    return cnt
+
+
+print(solution(437674, 3))
+print(solution(110011, 10))
