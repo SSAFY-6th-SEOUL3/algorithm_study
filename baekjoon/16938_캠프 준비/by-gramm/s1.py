8e687cde21a0470fdc1c13a9f27aec6a79588f15
by-gramm <ready2start@naver.com>
2022-01-30 19:12:14 +0900

    Upload: week15 | 캠프 준비

--- baekjoon/16938_캠프 준비/by-gramm/s1.py
+++ baekjoon/16938_캠프 준비/by-gramm/s1.py
@@ -1,1 +1,19 @@
-# 풀이 예정
+
+N, L, R, X = map(int, input().split())
+levels = [int(x) for x in input().split()]
+levels.sort()  # levels를 미리 정렬하여, current도 자동 정렬되도록 함.
+
+total_count = 0
+
+# bitmask 활용
+for i in range(1, 1 << N):
+    current = []
+
+    for j in range(N):
+        if i & (2 ** j):
+            current.append(levels[j])
+
+    if L <= sum(current) <= R and current[-1] - current[0] >= X:
+        total_count += 1
+
+print(total_count)
