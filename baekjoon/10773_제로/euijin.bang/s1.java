4947f23b93d34315b23240862a265c0b3affee88
euijinbang <euijin.bang@gmail.com>
2022-02-06 18:42:51 +0900

    Upload: Week16 | 제로

--- baekjoon/10773_제로/euijin.bang/s1.java
+++ baekjoon/10773_제로/euijin.bang/s1.java
@@ -10,19 +10,16 @@ public class BOJ10773 {
 
         ArrayList<Integer> data = new ArrayList<Integer>(LIMIT);
 
-        int sum = 0;
         for (int i = 0; i < K; i++) {
             int n = scanner.nextInt();
 
             if (n == 0) {
-                sum -= (int) data.get(data.size() - 1); // data[-1]
                 data.remove(data.size() - 1); // pop()
             } else {
-                sum += n;
                 data.add(n);
             }
         }
-
-        System.out.println(sum);
+        int total = data.stream().mapToInt(Integer::intValue).sum(); // stream
+        System.out.println(total);
     }
 }
---
+++ baekjoon/10773_제로/euijin.bang/s1.py
@@ -0,0 +1,12 @@
+K = int(input())
+data = []
+
+for _ in range(K):
+    n = int(input())
+
+    if n:
+        data.append(n)
+    else:
+        data.pop()
+
+print(sum(data))
