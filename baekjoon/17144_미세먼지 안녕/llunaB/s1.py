609e48215b733a3f75c2cef50dbe575824daacbd
euijinbang <euijin.bang@gmail.com>
2022-03-06 21:13:10 +0900

    Upload: Week19 | 17144_미세먼지 안녕(replace: 1018_체스판 다시 칠하기)

---
+++ baekjoon/17144_미세먼지 안녕/llunaB/s1.py
@@ -0,0 +1,32 @@
+"""
+replace : 1018_체스 칠하기
+"""
+
+N, M = map(int, input().split())
+
+arr = []
+count_list = []
+
+for _ in range(N):
+    arr.append(input())
+
+for i in range(N - 7):
+    for j in range(M - 7):
+        index1 = 0  # 'W'로 시작할 경우 바뀐 체스 판의 개수
+        index2 = 0  # 'B'로 시작할 경우 바뀐 체스 판의 개수
+        for a in range(i, i + 8):
+            for b in range(j, j + 8):
+                if (a + b) % 2 == 0:
+                    if arr[a][b] != 'W':
+                        index1 += 1
+                    if arr[a][b] != 'B':
+                        index2 += 1
+                else:
+                    if arr[a][b] != 'B':
+                        index1 += 1
+                    if arr[a][b] != 'W':
+                        index2 += 1
+
+        count_list.append(min(index1, index2))
+
+print(min(count_list))
